cbe4923126c1ef61456a9736b11f80046111dc2d
Vitor <torquatovitor68@gmail.com>
2024-03-05 08:57:40 +0100

    building

--- src/server.js
+++ src/server.js
@@ -1,13 +1,37 @@
+require('express-async-errors');
+
+const database = require('./database/sqlite')
+
 const express = require('express');
+
 
 
 const app = express();
 
 app.use(express.json());
 
+database();
+
 const routes = require('./routes');
 
 app.use(routes);
+
+const AppError = require('./utils/AppError');
+
+app.use((error,request,response,next) => {
+    if(error instanceof AppError){
+            return response.status(error.statusCode).json({
+                status: error,
+                message: error.message
+            })
+        }
+        console.error(error);
+
+        return response.status(500).json({
+            status: "error",
+            message: "Internal server error" 
+        });
+});
 
 
 const PORT = 3333;
