73b1a2b3e4443c894d8158b51af6ed0e6d8f640f
Vitor <torquatovitor68@gmail.com>
2024-04-08 12:12:03 +0200

    DiskStorage and uploadConfig

---
+++ src/config/upload.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const multer = require('multer');
+
+const crypto = require('crypto');
+
+
+
+const TPM_FOLDER  = path.resolve(__dirname, ".." ,"..", "tpm");
+
+const UPLOADS_FOLDER  = path.resolve(TPM_FOLDER,"upload");
+
+const MULTER = {
+    Storage: multer.diskStorage({
+        destination: TPM_FOLDER,
+        filename(request,file,callback){
+            const fileHash = crypto.randomBytes(10).toString("hex")
+            const fileName = `${fileHash}-${file.originalname}`
+
+            return callback(null, fileName);
+        }
+    })
+
+};
+
+module.exports = {
+    TPM_FOLDER,
+    UPLOADS_FOLDER,
+    MULTER
+};
